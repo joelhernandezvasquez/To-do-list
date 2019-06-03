3aae9e4702b755149e1cbaffa3065875a18dd247
Joel Hernandez <jhernandez98@outlook.es>
2019-06-03 09:32:52 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,153 @@
-function createTask()
-{
-    // getting the input value
-    const userInput = document.querySelector("input[type=text]");
-    const taskText = userInput.value.trim(); 
 
-    // Creating the elements dinamically 
-     // creating the div element, adding the styles and appending
-     if(taskText)
-     {
-     const parentContainer = document.querySelector(".task");
-     const divContainer = document.createElement("div");
-     divContainer.classList.add("task-item");
-     parentContainer.appendChild(divContainer);
+const rootendpoint = 'https://api.learnjavascript.today';
+const username ="joelhernandezvasquez";
+const password = "jya300591";
+
+// // //Creating user 
+// // zlFetch(`${rootendpoint}/users`,
+// // {
+// //     method:'post',
+
+// //     body:
+// //     {
+// //         username:`${username}`,
+// //         password :`${password}`
+// //     }
+// // })
+
+// .then(response=> console.log(response.body))
+// .catch(error => console.log(error))
+
+
+//Fetching tasks
+
+zlFetch(`${rootendpoint}/tasks`,{
+      username:`${username}`,
+       password:`${password}` 
+})
+.then(response=> 
+    {
+       const tasks = response.body;
+
+       tasks.forEach(task=>
+       {
+         fetchTask(task.name)
+       })
+       emptyTextStatement.textContent = " Your to do list is empty, Hurray!"
+    })
+
+
+function fetchTask(taskName)
+{
+    const parentContainer = document.querySelector(".task");
+    const divContainer = document.createElement("div");
+    divContainer.classList.add("task-item");
+    parentContainer.appendChild(divContainer);
 
      // Creating the checkbox
      const checkboxInput = document.createElement("input");
      checkboxInput.type = "checkbox";
 
-    // Creating the label 
+      // Creating the label 
     const labelInput = document.createElement("label");
     labelInput.classList.add("label-task");
-    labelInput.textContent = taskText;
+    labelInput.textContent = taskName;
 
-    // Creating the delete button
-    const deleteButton = document.createElement("input");
-    deleteButton.type = "image";
-    deleteButton.src = "cancel.svg";
-    deleteButton.classList.add("icon");
+     // Creating the delete button
+     const deleteButton = document.createElement("input");
+     deleteButton.type = "image";
+     deleteButton.src = "cancel.svg";
+     deleteButton.classList.add("icon");
+ 
+     // appending all elements to the div container
+     divContainer.appendChild(checkboxInput);
+     divContainer.appendChild(labelInput);
+     divContainer.appendChild(deleteButton);
+ 
+     hideParagraph();
+     positioningDeleteButton();
 
-    // appending all elements to the div container
-    divContainer.appendChild(checkboxInput);
-    divContainer.appendChild(labelInput);
-    divContainer.appendChild(deleteButton);
+}
 
-    hideParagraph();
-    positioningDeleteButton();
 
-    // clear the text input
-    userInput.value = "";
-    userInput.focus();
+function createTask()
+{
+    // getting the input value
+    const userInput = document.querySelector("input[type=text]");
+    const taskText = userInput.value.trim(); 
+    
+
+    // Creating the elements dinamically 
+     // creating the div element, adding the styles and appending
+     if(taskText)
+     {
+        buttonAddTask.textContent = "adding Task...";
+        buttonAddTask.classList.add("btn-task");
+        buttonAddTask.classList.add("btn");
+        buttonAddTask.setAttribute('disabled',true);
+        
+
+        // Saving the task to the database
+
+        zlFetch(`${rootendpoint}/tasks`,
+        {
+            method:'post',
+            
+            username:`${username}`,
+            password:`${password}`,
+
+            body:
+            {
+                name:taskText
+            }
+
+        })
+        .then(response=>
+            {
+                const parentContainer = document.querySelector(".task");
+                const divContainer = document.createElement("div");
+                divContainer.classList.add("task-item");
+                parentContainer.appendChild(divContainer);
+           
+                // Creating the checkbox
+                const checkboxInput = document.createElement("input");
+                checkboxInput.type = "checkbox";
+           
+               // Creating the label 
+               const labelInput = document.createElement("label");
+               labelInput.classList.add("label-task");
+               labelInput.textContent = taskText;
+           
+               // Creating the delete button
+               const deleteButton = document.createElement("input");
+               deleteButton.type = "image";
+               deleteButton.src = "cancel.svg";
+               deleteButton.classList.add("icon");
+           
+               // appending all elements to the div container
+               divContainer.appendChild(checkboxInput);
+               divContainer.appendChild(labelInput);
+               divContainer.appendChild(deleteButton);
+           
+               hideParagraph();
+               positioningDeleteButton();
+           
+               // clear the text input
+               userInput.value = "";
+               userInput.focus();
+            }
+            )
+            .catch(error=> console.log(error))
+            .finally(_=>{
+                
+                buttonAddTask.textContent = "Add task";
+                buttonAddTask.classList.add("btn-task");
+                buttonAddTask.classList.add("btn");
+                buttonAddTask.removeAttribute('disabled');
+            })
+      
+     
+   
 }
 
 }
